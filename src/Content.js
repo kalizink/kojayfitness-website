87e0d9f0d9c212cbb5f376b2ab5d3064352316e2
kalizink <kalizink65@gmail.com>
2024-01-30 05:15:55 -0600

    added trainerize section to content component

--- src/Content.js
+++ src/Content.js
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "./logo.png";
 import "./Content.css";
+import EPN1 from "./epn-logo.png";
 
 export default function Content() {
 	return (
 		<div className="Content mt-5">
+			{/* trainerize start */}
 			<div className="row">
 				<div className="col-sm-6 ">
 					<img src={logo} className="img-fluid" alt="no-rel" />
@@ -25,6 +27,28 @@ export default function Content() {
 					</div>
 				</div>
 			</div>
+			{/* trainerize end */}
+			{/* EPN start */}
+			<div className="row mt-5">
+				<div className="col-sm-6 ">
+					<img src={EPN1} className="img-fluid" alt="no-rel" />
+				</div>
+				<div className="col-sm-6 text-content">
+					<h2 className="text-center mt-3 line-height">
+						Use JBENNETT for 10% OFF
+					</h2>
+					<div className="text-center mt-5 line-height">
+						<a
+							href="https://ep-nutrition.com/"
+							target="blank"
+							title="EPN Supplements"
+							className="outside-link">
+							Go to Supplements
+						</a>
+					</div>
+				</div>
+			</div>
+			{/* EPN end */}
 		</div>
 	);
 }
