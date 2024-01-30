45cfe9cb5463d13c08d7d790fe10a6b88b754481
kalizink <kalizink65@gmail.com>
2024-01-30 05:05:18 -0600

    Edited responsiveness and removed manifest component because erros

--- src/Content.js
+++ src/Content.js
@@ -5,22 +5,26 @@ import "./Content.css";
 export default function Content() {
 	return (
 		<div className="Content mt-5">
-			<section>
-				<img src={logo} className="img-fluid" alt="no-rel" />
-				<h1 className="text-center mt-3">Trainerize</h1>
-				<h2 className="text-center mt-3">
-					Online training & nutrition programs
-				</h2>
-				<div className="text-center mt-5">
-					<a
-						href="https://www.trainerize.me/profile/kojayfitness/Jeremiah.Bennett/"
-						target="blank"
-						title="Go to trainerize"
-						className="outside-link">
-						Go To Trainerize
-					</a>
+			<div className="row">
+				<div className="col-sm-6 ">
+					<img src={logo} className="img-fluid" alt="no-rel" />
 				</div>
-			</section>
+				<div className="col-sm-6 text-content">
+					<h1 className="text-center mt-3 line-height">Trainerize</h1>
+					<h2 className="text-center mt-3 line-height">
+						Online training & nutrition programs
+					</h2>
+					<div className="text-center mt-5 line-height">
+						<a
+							href="https://www.trainerize.me/profile/kojayfitness/Jeremiah.Bennett/"
+							target="blank"
+							title="Go to trainerize"
+							className="outside-link">
+							Go To Trainerize
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
