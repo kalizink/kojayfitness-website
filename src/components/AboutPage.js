68bd8817bc2c06d0126563c2742f12635e75adc5
kalizink <kalizink65@gmail.com>
2024-02-13 15:59:59 -0600

    AboutPage: added images

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import NavbarComp from "./NavbarComp";
 import "./AboutPage.css";
+import jeremiah1 from "../images/jeremiah1.png";
+import jeremiah2 from "../images/jeremiah2.png";
+import jeremiah3 from "../images/jeremiah3.png";
 
 export default function AboutPage() {
 	return (
@@ -10,19 +13,40 @@ export default function AboutPage() {
 				<div className="header">
 					<h1 className="text-center">About Me</h1>
 					<h2 className="text-center">Personal Trainer from California</h2>
-					<p className="text-center">
+					<p>
 						{`My name is Jeremiah Bennett, a seasoned sports performance coach and personal trainer, dedicating my expertise to training since 2021. Armed with a Bachelor's degree in Exercise Science from Northern Arizona University and years of football training, I’ve worked with 100’s of athletes and individuals looking to become more explosive and help how to rebuild and prevent injuries. My passion lies in crafting challenging workouts that seamlessly weave together elements of balance, strength, mobility, and athleticism, ensuring each session propels you toward continuous improvement. Make the move and join me now to take your athleticism to the next level!`}
 					</p>
 				</div>
-				<div className="images">
+				<div className="images text-center">
 					<div className="row">
-						<div className="column">*** bench img ***</div>
+						<div className="column">
+							<img
+								src={jeremiah1}
+								alt="no-rel"
+								title="Jeremiah Bennett"
+								className="img-fluid"
+							/>
+						</div>
 					</div>
 					<div className="row">
-						<div className="column">*** muscles img ***</div>
+						<div className="column">
+							<img
+								src={jeremiah2}
+								alt="no-rel"
+								title="Jeremiah Bennett"
+								className="img-fluid"
+							/>
+						</div>
 					</div>
 					<div className="row">
-						<div className="column">*** 3 img ***</div>
+						<div className="column">
+							<img
+								src={jeremiah3}
+								alt="no-rel"
+								title="Jeremiah Bennett"
+								className="img-fluid"
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
 	return (
 		<div className="Header">
 			<h1 className="text-center">Jeremiah Bennett</h1>
-			<h2 className="text-center mt-2">
+			<h2 className="text-center">
 				Sports Performance Coach & Personal Trainer in Houston, TX
 			</h2>
 			<div className="navigation-links text-center mt-5">
